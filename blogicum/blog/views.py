66b65f8c085a50f5206ca08e3422f381b7916458
Ilya Kazakov <robotbotyourube@gmail.com>
2024-12-11 23:23:11 +0300

    second

---
+++ blogicum/blog/views.py
@@ -0,0 +1,47 @@
+from django.utils import timezone
+
+from django.shortcuts import render, get_object_or_404
+from .models import Post, Category
+
+
+def index(request):
+    template = "blog/index.html"
+    post_list = (
+        Post.objects.filter(
+            is_published=True,
+            category__is_published=True,
+            pub_date__lte=timezone.now(),
+        )
+        .order_by("-pub_date")[:5]
+        .select_related("category")
+    )
+    context = {"post_list": post_list}
+    return render(request, template, context)
+
+
+def post_detail(request, pk):
+    template = "blog/detail.html"
+    post = get_object_or_404(
+        Post,
+        pk=pk,
+        is_published=True,
+        pub_date__lte=timezone.now(),
+        category__is_published=True,
+    )
+    return render(request, template, {"post": post})
+
+
+def category_posts(request, category_slug):
+    template = "blog/category.html"
+    category = get_object_or_404(
+        Category, slug=category_slug, is_published=True
+    )
+    post_list = Post.objects.filter(
+        category=category,
+        is_published=True,
+        pub_date__lte=timezone.now()
+    ).order_by("-pub_date")
+    return render(
+        request, template, {"category": category,
+                            "post_list": post_list}
+    )
